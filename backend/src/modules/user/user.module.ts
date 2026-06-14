8050170d9bd57995539070f10672b489a60d87b6
Le Huu Huy <lehuuhuy211405@gmail.com>
2026-06-14 19:19:31 +0700

    fix add module permission to module user

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PermissionModule } from '../permission/permission.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]) , PermissionModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
